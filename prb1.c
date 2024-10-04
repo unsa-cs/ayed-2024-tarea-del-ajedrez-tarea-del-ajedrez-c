8bc22c3d0add0295044b359a819f55cabcddc745
JoaquinCastelo <jcasteloc@unsa.edu.pe>
2024-10-04 00:19:02 -0500

    Al estar compilando el archivo ejemplo.c nos dio curiosidad el compilar otro archivo con otro nombre el cual seria prb1.c al usar make ya no make HW=ejemplo sino que seria make HW=prb1 y logramos compilar todo el archivo

---
+++ prb1.c
@@ -0,0 +1,9 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  //char** blackKnight = reverse(knight);
+  //interpreter(blackKnight);
+  char** whitesquare = whiteSquare;
+  interpreter(whitesquare);
+}
