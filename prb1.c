fe82801b785290a913f596dc519cd5e059e839f7
gerald923 <gsupom@unsa.edu.pe>
2024-10-05 02:35:17 +0000

    lo que hicimos ahora fue hacer un join a un doble puntero a char que hacia otro join dentro de el y con esto conseguimos 4 cuadraditos de diferentes color, sin embargo en el terminal salio error llamado error MESA, ZINK y glx, no obstante el codigo compilo, y mostro la imagen

--- prb1.c
+++ prb1.c
@@ -6,6 +6,8 @@ void display(){
   char** whiteS = whiteSquare;
   char** prueba = repeatH(whiteS, 2);
   char** prueba2 = join(whiteS, blackS);
-  char** prueba3 = join(whiteS, blackS, whiteS, blackS, whiteS, blackS, whiteS, blackS);
-  interpreter(prueba3);
+  //char** prueba3 = join(whiteS, blackS, whiteS, blackS, whiteS, blackS, whiteS, blackS);
+  char** prueba4 = join(whiteS, blackS);
+  char** prueba5 = join(prueba2, prueba4);
+  interpreter(prueba5);
 }
