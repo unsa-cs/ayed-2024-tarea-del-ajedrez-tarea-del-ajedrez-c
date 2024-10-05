69955452180e28a4576b2239f0c4d0357e38c305
KarlaCornejo <kcornejop@KARLITA>
2024-10-04 22:38:45 -0500

    Probamos la función flipH para hacer el ejercicio 3, no sabemos AÚN para que sirve

---
+++ prb3.c
@@ -0,0 +1,13 @@
+#include "chess.h"
+#include "figures.h"
+ 
+void display(){
+  char** blackS = reverse(whiteSquare);
+  char** whiteS = whiteSquare;
+  char** prueba2 = join(whiteS, blackS);
+  char** prueba7 = repeatH(prueba2, 4);
+  char** prueba8 = reverse(prueba7);
+  char** prueba9 = flipH(prueba7);
+
+  interpreter(prueba9);
+}
