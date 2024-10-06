8296357494752edaebbe327e871aa7d994dfafb1
gerald923 <gsupom@unsa.edu.pe>
2024-10-06 03:29:26 +0000

    profe al final al compilar usando make nos sale sin error, ahora el problema es el ejecutable, nos dice segmentation fault, vamos a investigar dicho error

--- prb4.c
+++ prb4.c
@@ -8,17 +8,18 @@ struct figureChess{
   char** arrayfigure[MAX_SIZE];
   char** figures;
 };
-void chess(figureChess c){
-  c.arrayfigure[1] = rook;
-  c.arrayfigure[2] = knight;
-  c.arrayfigure[3] = bishop;
-  c.arrayfigure[4] = queen;
-  c.arrayfigure[5] = king;
-  c.arrayfigure[6] = bishop;
-  c.arrayfigure[7] = knight;
-  c.arrayfigure[8] = rook;
+void chess(figureChess* c){
+  c->arrayfigure[1] = rook;
+  c->arrayfigure[2] = knight;
+  c->arrayfigure[3] = bishop;
+  c->arrayfigure[4] = queen;
+  c->arrayfigure[5] = king;
+  c->arrayfigure[6] = bishop;
+  c->arrayfigure[7] = knight;
+  c->arrayfigure[8] = rook;
 }
 figureChess fila(figureChess c){
+  c.figures = NULL;
   for(int i = 0; i < MAX_SIZE; i++){
     c.figures = join(c.figures, c.arrayfigure[i]);
   }
@@ -52,7 +53,7 @@ void display(){
   //printf("tamaño de una figura es = %d\n", getSize(knight));
   char** prueba17 = join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook);  
   figureChess figure1;
-  chess(figure1);
+  chess(&figure1);
   fila(figure1);
   interpreter(figure1.figures);
 
