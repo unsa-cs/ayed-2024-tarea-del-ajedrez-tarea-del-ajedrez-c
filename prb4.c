e5f26590a7fe8372b48331187d4af80d6e21f45c
Lizardo-Castillo <lcastillol@unsa.edu.pe>
2024-10-05 20:47:53 -0500

    Usamos la funcion en un caso extraordinario, porq en este caso le prueba8 es una fila de 8 casilleros y al poner al caballo usando el superImpose con la prueba 8, se vio q solo retorna un casillero con el caballo, mas no con todos los casilleros. Pero tambien al hacerlo al revez, si imprimio la imagen de los 8 casilleros, entonces la cantidad de imagenes q debe de retornar dependera del primera parametro

--- prb4.c
+++ prb4.c
@@ -4,10 +4,13 @@
 void display(){
   char** blackS = reverse(whiteSquare);
   char** whiteS = whiteSquare;
+  char** knightB = reverse(knight);
   char** prueba2 = join(whiteS, blackS);
   char** prueba7 = repeatH(prueba2, 4);
   char** prueba8 = reverse(prueba7);
   char** prueba9 = knight;
   char** prueba10 = superImpose(whiteSquare, reverse(knight));
-  interpreter(prueba10);
+  char** prueba11 = superImpose(knight, whiteSquare);
+  char** prueba12 = superImpose(knightB, prueba8);
+  interpreter(prueba12);
 }
