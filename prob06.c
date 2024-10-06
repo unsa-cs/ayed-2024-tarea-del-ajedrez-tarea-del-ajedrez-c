3fc889ae22fe25aa2033dd4f6467c27660aaa5b7
Lizardo-Castillo <lcastillol@unsa.edu.pe>
2024-10-06 12:24:48 -0500

    Se hizo un ordenamiento de las variables para q sea mas ordenado y se uso algunas caracteristicas del ejercicio anterior como el struct usado. En este caso se uso el join para hacer la fila de peones y con esto se junto con la funcion up las figuras con los peones

--- prob06.c
+++ prob06.c
@@ -1,19 +1,57 @@
 #include "chess.h"
 #include "figures.h"
+#include "stdio.h"
+#include <stdlib.h>
+#define MAX_SIZE 8
+
+struct figureChess{
+  char** arrayfigure[MAX_SIZE];
+  char** figures;
+};
+
+void chess(figureChess* box){
+  box->arrayfigure[0] = rook;
+  box->arrayfigure[1] = knight;
+  box->arrayfigure[2] = bishop;
+  box->arrayfigure[3] = queen;
+  box->arrayfigure[4] = king;
+  box->arrayfigure[5] = bishop;
+  box->arrayfigure[6] = knight;
+  box->arrayfigure[7] = rook;
+}
+
+figureChess row(figureChess box){
+  box.figures = rook;
+  for(int i = 1; i < MAX_SIZE; i++){
+    box.figures = join(box.figures, box.arrayfigure[i]);
+  }
+  return box;
+}
 
 void display(){
-  char** blackKnight = reverse(knight);
-  char** prueba1 = pawn;
+  //Squares
   char** blackS = reverse(whiteSquare);
   char** whiteS = whiteSquare;
-  char** prueba2 = join(whiteS, blackS);
-  char** prueba7 = repeatH(prueba2, 4);
-  char** prueba8 = reverse(prueba7);
-
-  for(int i = 0; i < 3; i++){
-    prueba1 = join(prueba1, prueba1);
-  }
-  char** prueba3 = superImpose(prueba1, prueba7);
-  interpreter(prueba3);
+  char** joinWhiteBlack = join(whiteS, blackS);
+  // Row Chess Square
+  char** rowFirst = repeatH(joinWhiteBlack, 4);
+  char** rowFirstReverse = reverse(rowFirst);
+  //Figures
+  figureChess rowStart;
+  chess(&rowStart);
+  rowStart = row(rowStart);
+  //Only Figures
+  char** rowFiguresStart = rowStart.figures;
+  char** rowFiguresStartReverse = reverse(rowFiguresStart);
+  //With Square
+  char** rowStartBox = superImpose(rowStart.figures, rowFirstReverse);
+  char** rowStartBoxReverse = superImpose(reverse(rowStart.figures), rowFirstReverse);
+  //Row Pawn    
+  char** rowPawn = join(join(join(join(join(join(join(pawn, pawn), pawn), pawn), pawn), pawn), pawn), pawn);
+  char** rowPawnReverse = reverse(rowPawn);
+  //Figures White
+  char** figuresWhite = up(rowFiguresStart, rowPawn);
+  //Interpreter
+  interpreter(figuresWhite);
 }
 
